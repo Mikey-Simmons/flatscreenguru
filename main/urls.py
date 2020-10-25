91f90b8ff1370b83f54a44bca45fe73d6511ac24
Mikey-Simmons <mikeysimmons425@gmail.com>
2020-10-25 10:47:36 -0700

    Initial commit

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path     
+from . import views
+urlpatterns = [
+    path('', views.index),
+    path('jobinquiry',views.job_inquiry),
+    path('contact',views.contact_us),
+    path('gallery',views.gallery_page),
+    path('addjob',views.addjob),
+    path('submitted',views.submitjob),
+    path('pricing',views.price_page)
+]
---
+++ main/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from .models import Job
+
+def index(request):
+    return render(request,'index.html') 
+def job_inquiry(request):
+    return render(request, 'job.html')
+def contact_us(request):
+    return render(request,'contact.html')
+def gallery_page(request):
+    return render(request,'slider.html')
+def addjob(request):
+    all_jobs = Job.objects.all()
+    new_job = Job.objects.create(
+        customer_name = request.POST['customer_name'],
+        job_desc = request.POST['job_desc'],
+        location = request.POST['location'],
+        email_address = request.POST['email'],
+        phone_number = request.POST['phone_number']
+    )
+    return  redirect('/submitted')
+def submitjob(request):
+    return render(request,'submit_page.html')
+def price_page(request):
+    return render(request,'pricing.html')
