a3b1b6c0de0b072d2a59115becd4181d5bebb02d
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2020-11-17 21:16:39 -0800

    added validations, some products
    added basic validations to job page, added links to products page

--- main/models.py
+++ main/models.py
@@ -3,40 +3,17 @@ from django.db import models
 from django.db import models
 import re 
 
-class UserManager(models.Manager):
-    def basic_validator(self,postData):
-        errors = {}
-        if len(postData['first_name']) < 3:
-            errors['first_name'] = "First name needs to be at least 3 characters"
-        if len(postData['last_name']) < 3:
-            errors['last_name'] = "Last name needs to be at least 3 characters"
-        if len(postData['email']) < 8:
-            errors['email'] = "Email needs to be at least 8 characters"
-        if len(postData['password']) < 8:
-            errors['password'] = "Password needs to be at least 8 characters"
-        if postData['password'] != postData['confirm_password']:
-            errors['invalid_password'] ="Passwords do not match!"
-        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-        if not EMAIL_REGEX.match(postData['email']):    # test whether a field matches the pattern            
-            errors['email'] = "Invalid email address!"
-        return errors
-    def login_validator(self,postData):
-        errors = {}
-        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-        if not EMAIL_REGEX.match(postData['email']):    
-            errors['email'] = "Invalid email address!"
-        if len(postData['email']) < 8:
-            errors['email'] = "Email needs to be at least 8 characters"
-        return errors
+
 class JobManager(models.Manager):
-        def job_validator(self,postData):
-            errors = {}
-            if len(postData['customer_name']) < 2:
-                errors['customer_name'] = "Name must be more than 2 characters"
-            if len(postData['job_desc']) < 5:
-                errors['job_desc'] = "Description must be more than 5 characters"
-            if len(postData['location']) < 5:
-                errors['location'] = "Location must be more than 5 characters"
+    def job_validator(self,postData):
+        errors = {}
+        if len(postData['customer_name']) < 2:
+            errors['customer_name'] = "Name must be more than 2 characters"
+        if len(postData['job_desc']) < 5:
+            errors['job_desc'] = "Description must be more than 5 characters"
+        if len(postData['location']) < 5:
+            errors['location'] = "Location must be more than 5 characters"
+        return errors
             
 class Job(models.Model):
     customer_name = models.CharField(max_length=255)
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Job
-
+from django.contrib import messages
 def index(request):
     return render(request,'index.html') 
 def job_inquiry(request):
@@ -10,14 +10,23 @@ def contact_us(request):
 def gallery_page(request):
     return render(request,'slider.html')
 def addjob(request):
-    all_jobs = Job.objects.all()
-    new_job = Job.objects.create(
-        customer_name = request.POST['customer_name'],
-        job_desc = request.POST['job_desc'],
-        location = request.POST['location'],
-        email_address = request.POST['email'],
-        phone_number = request.POST['phone_number']
-    )
+    errors = Job.objects.job_validator(request.POST)
+        # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        return redirect('/jobinquiry')
+    else:
+        all_jobs = Job.objects.all()
+        new_job = Job.objects.create(
+            customer_name = request.POST['customer_name'],
+            job_desc = request.POST['job_desc'],
+            location = request.POST['location'],
+            email_address = request.POST['email'],
+            phone_number = request.POST['phone_number']
+        )
     return  redirect('/submitted')
 def submitjob(request):
     return render(request,'submit_page.html')
