c1d5ae7ac0ad53269aed7c38e04c25ec53a13603
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2020-11-23 17:12:12 -0800

    small changes
    nothing major

--- main/models.py
+++ main/models.py
@@ -13,6 +13,11 @@ class JobManager(models.Manager):
             errors['job_desc'] = "Description must be more than 5 characters!"
         if len(postData['location']) < 5:
             errors['location'] = "Location must be more than 5 characters!"
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):    # test whether a field matches the pattern            
+            errors['email'] = "Invalid email address!"
+        if len(postData['email']) < 8:
+            errors['email'] = "Email needs to be at least 8 characters"
         return errors
             
 class Job(models.Model):
